1164d124b6a0984eb26adb43f95d80d9ac61b643
kkeian <kaserman22@gmail.com>
2025-02-16 00:20:05 -0500

    Add project files.

---
+++ Library.fs
@@ -0,0 +1,45 @@
+﻿namespace workscheduling
+
+module Scheduling =
+    type Day =
+        {
+            Date : System.DateOnly
+            TimeBlock : System.TimeOnly
+        }
+
+    // use to instantiate Schedule Week
+    let makeWeekDays (startDate: System.DateOnly ) =
+        [| for i in 0 .. 6 -> startDate.AddDays i |]
+
+    type Address =
+        {
+            Number : uint
+            StreetName : string
+            SuiteNumber : uint
+            Floor : int
+            City : string
+            State : string
+            ZipCode : uint
+        }
+
+    type Location =
+        {
+            Name : string
+            Addr : Address
+            OpenHours : Day array
+        }
+
+    type Schedule =
+        {
+            Week : Day array
+            Site : Location
+        }
+
+    type Employee =
+        { 
+            FirstName : string
+            LastName : string
+            Availability : Schedule
+            WeekSchedule : Schedule
+            EmployeeNumber : uint64
+        }
