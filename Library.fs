3ebc1cfa80d233ef20765aaf8c7ec4933687530d
kkeian <kaserman22@gmail.com>
2025-02-16 00:38:34 -0500

    Add position field for employees and position record with unique id generator

--- Library.fs
+++ Library.fs
@@ -1,10 +1,16 @@
 ﻿namespace workscheduling
 
 module Scheduling =
+    type TimeSpan =
+        {
+            Start : System.TimeOnly
+            End : System.TimeOnly
+        }
+
     type Day =
         {
             Date : System.DateOnly
-            TimeBlock : System.TimeOnly
+            TimeBlock : TimeSpan
         }
 
     // use to instantiate Schedule Week
@@ -34,6 +40,20 @@ module Scheduling =
             Week : Day array
             Site : Location
         }
+    
+    type Position =
+        {
+            Name : string
+            Id : uint32 seq // should be unique
+        }
+
+    // generator function that returns next valid Id
+    let validIds = seq { for i in 1u .. System.UInt32.MaxValue -> i }
+    let makePosition name =
+        {
+            Name = name
+            Id = validIds
+        }
 
     type Employee =
         { 
@@ -42,4 +62,5 @@ module Scheduling =
             Availability : Schedule
             WeekSchedule : Schedule
             EmployeeNumber : uint64
+            JobTitle : Position
         }
